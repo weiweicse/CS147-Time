5e359804f7fe9f20758b41cca7c4e3e0370da29e
Kavin Yao <kavinyao@gmail.com>
2014-02-10 22:50:17 -0800

    add view toggler to usage view

--- static/js/usage.js
+++ static/js/usage.js
@@ -1,20 +1,21 @@
 $(function() {
     // boilerplate function
+    // currently generates random tasks
     function getTasks() {
         var tasks = [
             {
                 name: 'Clean House',
-                minutes: 100,
+                minutes: Math.random()*100,
                 color: '#F38630'
             },
             {
                 name: 'CS147',
-                minutes: 200,
+                minutes: Math.random()*400,
                 color: '#E0E4CC'
             },
             {
                 name: 'Feed Cat',
-                minutes: 400,
+                minutes: Math.random()*200,
                 color: '#69D2E7'
             }
         ];
@@ -22,23 +23,47 @@ $(function() {
         return tasks;
     }
 
-    var tasks = getTasks();
-    var data = tasks.map(function(t) {
-        return {
-            value: t.minutes,
-            color: t.color
-        };
-    });
-
     var $chart = $('#pie-chart');
-    var width = $chart.closest('.container').width();
-    $chart.attr('width', width).attr('height', width);
-
-    var ctx = $chart[0].getContext('2d');
-    var chart = new Chart(ctx).Pie(data);
-
     var $legend = $('#task-legends');
-    tasks.map(function(t) {
-        $legend.append('<li><button type="button" class="btn btn-xs" style="color:rgba(0,0,0,0);background-color:' + t.color + ';">X</button>' + t. name + '</li>');
+
+    function updateGraph(tasks) {
+        var data = tasks.map(function(t) {
+            return {
+                value: t.minutes,
+                color: t.color
+            };
+        });
+
+        var width = $chart.closest('.container').width();
+        $chart.attr('width', width).attr('height', width);
+
+        var ctx = $chart[0].getContext('2d');
+        var chart = new Chart(ctx).Pie(data);
+
+        $legend.fadeOut().promise().done(function() {
+            $legend.html('');
+
+            tasks.map(function(t) {
+                $legend.append('<li><button type="button" class="btn btn-xs" style="color:rgba(0,0,0,0);background-color:' + t.color + ';">X</button>' + t. name + '</li>');
+            });
+
+            $legend.fadeIn();
+        });
+    }
+
+    var $toggler = $('#view-toggler');
+    $toggler.on('click', 'a', function() {
+        // do not re-draw on active item
+        var $active_item = $toggler.find('.active');
+        if ($active_item.find('a')[0] === this) return;
+
+        $active_item.removeClass('active');
+        $(this).closest('div').addClass('active');
+
+        var tasks = getTasks();
+        updateGraph(tasks);
     });
+
+    // manually trigger a click to show daily view
+    $toggler.find('a').first().click();
 });
