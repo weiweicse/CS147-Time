837406512ddec431f8723b7057acd91cf3d31a40
Kavin Yao <kavinyao@gmail.com>
2014-02-13 12:01:06 -0800

    refactor history items as a template
    so we can reuse it later

--- routes/core.js
+++ routes/core.js
@@ -14,6 +14,27 @@ exports.usage = function(req, res) {
     res.render('usage');
 };
 
+var tasks = ['Feed Cat', 'CS147', 'Exercise', 'Running', 'Studio', 'Movie'];
+
+function randomEvents(dates) {
+    return dates.map(function(d) {
+        var events = [], num_events = Math.floor(Math.random()*3+2);
+        while (num_events--) events.push({
+            name: tasks[Math.floor(Math.random()*tasks.length)],
+            start: '09:10AM',
+            end: '11:30AM'
+        });
+
+        return {
+            date: d,
+            events: events
+        };
+    });
+}
+
 exports.history = function(req, res) {
-    res.render('history');
+    var dates = ['2014-02-13', '2014-02-12', '2014-02-11', '2014-02-10'];
+    res.render('history', {
+        items: randomEvents(dates)
+    });
 };
