5a828d7151cbe4cd45289f265751bd7220b79ddd
Kavin Yao <kavinyao@gmail.com>
2014-02-13 01:26:30 -0800

    componentize help

--- static/js/index.js
+++ static/js/index.js
@@ -40,22 +40,4 @@ $(function() {
     
     setInterval(updateProgress, 60000);    
     setInterval(updateScreen, 1000);
-
-    // Set up help
-    var viewport_width = $(window).width();
-    var viewport_height = $(window).height();
-    var $panel = $('#help-panel').css('width', viewport_width*0.8);
-    // show it outside viewport to get proper height
-    $panel.css('left', viewport_width*0.1).css('top', -9999).show();
-    var top = (viewport_height - $panel.outerHeight()) / 2;
-    $panel.hide().css('top', top);
-
-    $('#help a').on('click', function(e) {
-        e.preventDefault();
-        $panel.fadeIn();
-    });
-
-    $panel.on('click', '.panel-title', function() {
-        $panel.fadeOut();
-    });
 });
