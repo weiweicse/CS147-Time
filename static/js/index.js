c10b45fe3fa35433fe20858231e76f9130798426
Kavin Yao <kavinyao@gmail.com>
2014-02-13 23:54:25 -0800

    fix #2

--- static/js/index.js
+++ static/js/index.js
@@ -4,9 +4,14 @@ $(function() {
         // TODO: more robust query test
         var $msg = $('#success-msg');
         var height = $msg.outerHeight();
-        $msg.css('top', -height).show().animate({top: 0});
+        /*
+         * Note: animate from bottom instead from top (which is more
+         *       idiomatic in iOS native apps) because the status bar
+         *       text seems always on top.
+         */
+        $msg.css('bottom', -height).show().animate({bottom: 0});
         setTimeout(function() {
-            $msg.animate({top: -height});
+            $msg.animate({bottom: -height});
         }, 2000);
     }
 
