fe2d5a45d445d19b015dae9e9ffa4fe5d1380fea
Wei Wei <fraternite@live.cn>
2014-02-20 19:14:21 -0800

    populate data to trend view

--- static/js/trend.js
+++ static/js/trend.js
@@ -12,14 +12,37 @@ $(document).ready(function() {
 });
 
 function createChart() {
-    var data = {
-        labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-        datasets: [{
-            fillColor: "#5abedb",
-            strokeColor: "#5abedb",
-            data: [3, 2, 2, 5, 3, 3, 4]
-        }]
-    };
+    // get current time
+    var now = new Date();
+    now.setHours(0);
+    now.setMinutes(0);
+    now.setSeconds(0);
+    // set data
+    var data = {};
+    data.labels = [];
+    // init labels
+    var starttime = new Date();
+    starttime.setDate(now.getDate() - 29);
+    var cnt = 0;
+    for (var d = starttime; d <= now; d.setDate(d.getDate() + 1)) {
+        if (cnt % 5 === 0) {
+            data.labels.push(d.toString().substring(3,10));
+        } else {
+            data.labels.push("  ");
+        }
+        cnt++;
+    }
+    console.log(data.labels);
+    data.datasets = [{}];
+    data.datasets[0].fillColor = "#5abedb";
+    data.datasets[0].strokeColor = "#5abedb";
+    data.datasets[0].data = [];
+    // get data from json api
+    $.get('/api/trend', function(d) {
+        console.log(data);
+        data.datasets[0].data = d;
+    });
+    console.log(data);
     var cht = document.getElementById('barContainer');
     var ctx = cht.getContext('2d');
     var barChart = new Chart(ctx).Bar(data, {
